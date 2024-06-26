c74539c6f5e6392e258cb05b6aa7a5ccb0ed2c01
zigzag1001 <peter.saburov@gmail.com>
2024-06-26 05:54:30 +0000

    wilder

--- init.lua
+++ init.lua
@@ -181,6 +181,32 @@ require('lazy').setup({
   },
   { 'zbirenbaum/copilot-cmp', opts = {} },
   { 'nvim-lualine/lualine.nvim', opts = {} },
+  {
+    'gelguy/wilder.nvim',
+    config = function()
+      require('wilder').setup {
+        modes = {
+          '/',
+          '?',
+          ':',
+          '!',
+        },
+      }
+      require('wilder').set_option(
+        'renderer',
+        require('wilder').popupmenu_renderer(require('wilder').popupmenu_palette_theme {
+          -- 'single', 'double', 'rounded' or 'solid'
+          -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+          border = 'rounded',
+          max_height = '25%', -- max height of the palette
+          min_height = '40%', -- set to the same as 'max_height' for a fixed height window
+          max_width = '60%', -- max width of the palette
+          prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
+          reverse = 0, -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+        })
+      )
+    end,
+  },
 
   -- KICKSTART STUFFFFF
 
@@ -773,6 +799,9 @@ require('lazy').setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
+
+      -- animate
+      require('mini.animate').setup()
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
